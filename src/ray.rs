4898d027ea3a2321148c6d7a87e1519f84e10f19
John Alberse <alberse.john@gmail.com>
2022-12-14 15:15:10 -0600

    Create ray_color()

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,6 @@
-use glam::Vec3;
+use glam::{vec3, Vec3};
+
+use crate::color::Color;
 
 struct Ray {
     pub origin: Vec3,
@@ -12,5 +14,11 @@ impl Ray {
 
     pub fn at(&self, t: f32) -> Vec3 {
         self.origin + t * self.direction
+    }
+
+    pub fn ray_color(&self) -> Color {
+        // TODO For now, this just returns something which will make a gradient. We will actually do ray tracing here eventually!
+        let t = 0.5 * (self.direction.normalize().y + 1.0);
+        ((1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0)).into()
     }
 }
