75fc2f87c694188d5ac3c1197928307c60c77b3d
John Alberse <alberse.john@gmail.com>
2022-12-15 01:02:54 -0600

    Add origin to Camera

--- src/camera.rs
+++ src/camera.rs
@@ -3,14 +3,21 @@ use crate::ray::Ray;
 use glam::Vec3;
 
 pub struct Camera {
+    origin: Vec3,
     viewport_height: f32,
     viewport_width: f32,
     focal_length: f32,
 }
 
 impl Camera {
-    pub fn new(viewport_height: f32, viewport_width: f32, focal_length: f32) -> Camera {
+    pub fn new(
+        origin: Vec3,
+        viewport_height: f32,
+        viewport_width: f32,
+        focal_length: f32,
+    ) -> Camera {
         Camera {
+            origin,
             viewport_height,
             viewport_width,
             focal_length,
@@ -18,14 +25,13 @@ impl Camera {
     }
 
     pub fn get_ray(&self, u: f32, v: f32) -> Ray {
-        let origin = Vec3::ZERO;
         let horizontal = Vec3::new(self.viewport_width, 0.0, 0.0);
         let vertical = Vec3::new(0.0, self.viewport_height, 0.0);
         let lower_left_corner =
             Vec3::ZERO - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, self.focal_length);
         Ray::new(
-            origin,
-            lower_left_corner + u * horizontal + v * vertical - origin,
+            self.origin,
+            lower_left_corner + u * horizontal + v * vertical - self.origin,
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,12 @@ use sphere::Sphere;
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let viewport_height = 2.0;
-    let camera = Camera::new(viewport_height, aspect_ratio * viewport_height, 1.0);
+    let camera = Camera::new(
+        Vec3::ZERO,
+        viewport_height,
+        aspect_ratio * viewport_height,
+        1.0,
+    );
     let renderer = Renderer::from_aspect_ratio(400, 16.0 / 9.0);
 
     let sphere = Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5));
