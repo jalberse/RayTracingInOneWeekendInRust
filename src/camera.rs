0b2f6a65b2037446ac3e60f9d4da6167b19e735e
John Alberse <alberse.john@gmail.com>
2022-12-15 01:05:47 -0600

    Refactor Camera
    Refactor Camera to do some calculations in new() and store the results,
    rather than recalculating from the input values each call to
    get_ray().

--- src/camera.rs
+++ src/camera.rs
@@ -4,9 +4,9 @@ use glam::Vec3;
 
 pub struct Camera {
     origin: Vec3,
-    viewport_height: f32,
-    viewport_width: f32,
-    focal_length: f32,
+    horizontal: Vec3,
+    vertical: Vec3,
+    lower_left_corner: Vec3,
 }
 
 impl Camera {
@@ -16,22 +16,22 @@ impl Camera {
         viewport_width: f32,
         focal_length: f32,
     ) -> Camera {
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            Vec3::ZERO - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
         Camera {
             origin,
-            viewport_height,
-            viewport_width,
-            focal_length,
+            horizontal,
+            vertical,
+            lower_left_corner,
         }
     }
 
     pub fn get_ray(&self, u: f32, v: f32) -> Ray {
-        let horizontal = Vec3::new(self.viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, self.viewport_height, 0.0);
-        let lower_left_corner =
-            Vec3::ZERO - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, self.focal_length);
         Ray::new(
             self.origin,
-            lower_left_corner + u * horizontal + v * vertical - self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
         )
     }
 }
