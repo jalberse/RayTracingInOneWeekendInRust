6c943fd584da4e654e3ec01271e7022180d1daf4
John Alberse <alberse.john@gmail.com>
2022-12-18 16:04:20 -0600

    Use Srgb in Renderer
    This has two main effects:
    
    1. The gamma correction is now different, using the standard sRGB
    format with a gamma of 2.2 rather than rolling our own gamma correction
    with a gamma of 2.0 (sqrt). This will change the perceived values of
    renders.
    
    2. This will be more maintainable. We ensure via the type system
    through the palette crate, that operations (adding, dividing, etc)
    on our colors are done in a linear color space, rather than our
    gamma corrected color space. We also allow a properly vetted
    library handle color spaces and conversions rather than rolling
    our own.
    
    For now, only the Renderer itself makes use of the palette crate
    and color types. The ray casting algorithm itself will continue
    to use raw vectors. This is a deliberate choice to avoid unnecessary
    boilerplate code for managing the color types and color spaces within
    the ray casting algorithm, since the entire module will always only
    deal with a linear color space. It makes more sense to begin enforcing
    color space properties only once we reach the rendering logic.
    
    In the future, we might have the Material API take sRgb for albedo
    rather than a DVec3 if that seems useful. But for now, the major
    utility is ensuring the renderer is handling color spaces correctly.
    
    This resolves #1.

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use glam::{dvec3, DVec3};
+use palette::Srgb;
 use rand::Rng;
 
 pub fn near_zero(vec: &DVec3) -> bool {
@@ -13,4 +14,10 @@ pub fn random_in_unit_disk() -> DVec3 {
             return p;
         }
     }
+}
+
+pub fn srgb_from_dvec3(vec: DVec3) -> Srgb {
+    // Our colors from ray tracing are already in linear rgb space, so
+    // we make no conversions.
+    Srgb::from_components((vec.x as f32, vec.y as f32, vec.z as f32))
 }
